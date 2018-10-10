21c4b100227a81ee4394c7844dda12dba3edfb22
Andrea <andreamayhirji@gmail.com>
2018-10-10 07:08:30 +0000

    added a section for posting tweeted content in the HTML, added styles in the CSS.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,12 +7,12 @@ $(document).ready(function () {
     let tweetText = document.querySelector(".new-tweet textarea");
     const maxChar = 140;
 
-    $(tweetText).on("keyup", function (event) {
+    $(tweetText).on("input", function (event) {
         const inputText = $(this).val();
         const charCount = inputText.length;
         const totalChar = maxChar - charCount;
         $(".counter").text(totalChar);
-
+console.log('HEY IM THE CHAR COUNT!',charCount);
         if (totalChar <= 0) {
             $(".counter").addClass("counter-negative")
         } else {
