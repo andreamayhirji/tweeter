489332ab51ddb71d3593b45f02bb6fec529f3d53
Andrea <andreamayhirji@gmail.com>
2018-10-11 04:21:01 +0000

    added an alert if the character counr goes below 0, and disabled the submit button.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -144,7 +144,6 @@ $(document).ready(function () {
             }
         })
 
-
     });
 
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -14,6 +14,8 @@ $(document).ready(function () {
         $(".counter").text(totalChar);
         if (totalChar <= 0) {
             $(".counter").addClass("counter-negative")
+            alert('TOO MANY CHARACTERS!');
+            $("#submitTweet").attr("disabled", true);
         } else {
             $(".counter").removeClass("counter-negative")
         }
